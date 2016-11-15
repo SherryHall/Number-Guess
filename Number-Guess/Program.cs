48556787e3e9bf6620d0d4143b536aac61d1f381
SherryHall <slhall222@gmail.com>
2016-11-15 15:35:31 -0500

    Inital Commit

---
+++ Number-Guess/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Guess
+{
+	class Program
+	{
+		Random rnd = new Random();
+		int numToGuess = rnd.Next(1, 100); // creates a number between 1 and 100
+		
+
+		static void Main(string[] args)
+		{
+			var wantToPlay = true;
+
+			while (wantToPlay)
+			{
+				Random rnd = new Random();
+				int numToGuess = rnd.Next(1, 100); // creates a number between 1 and 100
+
+
+
+			}
+		}
+	}
+}
