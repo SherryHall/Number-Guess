e7c162db3213b7462ef2b222050ddfcf7a23b0cf
SherryHall <slhall222@gmail.com>
2016-11-15 17:51:53 -0500

    Add logic to display failed guesses

--- Number-Guess/Program.cs
+++ Number-Guess/Program.cs
@@ -21,18 +21,19 @@ namespace Number_Guess
 				var guesses = new int[5];
 				var results = new string[5];
 				var guessCount = 0;
+				var maxGuesses = 5;
 				var goodGuess = false;
 
 				Console.WriteLine("Guess a number between 1 and 100.  You get 5 tries");
-				while ((!goodGuess) && (guessCount < 5))
+				while ((!goodGuess) && (guessCount < maxGuesses))
 				{
 					bool isAnInt = false;
 					var guessNum = 0;
-					int randomInt = 0;
 
 					Console.Write("Make a Guess: ");
 					var userEntry = Console.ReadLine();
 					isAnInt = int.TryParse(userEntry, out guessNum);
+
 					// if number entered equals number to guess you win
 					if (guessNum == numToGuess)
 					{
@@ -47,33 +48,41 @@ namespace Number_Guess
 						if ((guessNum < 1) || (guessNum > 100))
 						{
 							Console.WriteLine("Your guess is not in the specified range. If not an integer, considered 0");
+							results[guessCount] = "invalid";
 						}
 						else if (guessNum < numToGuess)
 						// if entry is less than the number to guess it is too low. 
 						{
 							Console.WriteLine("Your guess is too low");
-							results[guessCount] = "low";
+							results[guessCount] = "too low";
 						}
 						else
 						// The entry is more than the number to guess so it is too high. 
 						{
 							Console.WriteLine("Your guess is too high");
-							results[guessCount] = "high";
+							results[guessCount] = "too high";
 						}
 
 						guessCount++;
 					}
-
-					Console.Write("Press any key to continue ");
-					Console.ReadLine();
-
-
 				}
-
-
-
-
-
+				// You have used the max number of guesses and they were all wrong. Display failed guesses.
+				if (!goodGuess)
+				{
+					Console.WriteLine("\nYou have reached the guess limit. You Lose!!!");
+					Console.WriteLine($"The correct number was {numToGuess}");
+					Console.WriteLine("You made the following guesses");
+					for (int i = 0; i < 5; i++)
+					{
+						Console.WriteLine($"Guess #{i + 1} was {guesses[i]}, which was {results[i]}");
+					}
+				}
+				Console.Write("\nDo you want to play again? (Y/N)  ");
+				var answer = Console.ReadLine();
+				if (answer == "N")
+				{
+					wantToPlay = false;
+				}
 
 
 			}
